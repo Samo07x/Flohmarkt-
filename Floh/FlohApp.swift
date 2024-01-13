4d4638eff41665ad40f884b119121e47f343306f
Samo <abdussamedsen@icloud.com>
2024-01-13 15:49:59 +0100

    Initial Commit

---
+++ Floh/FlohApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlohApp.swift
+//  Floh
+//
+//  Created by Abdussamed Sen on 13.01.24.
+//
+
+import SwiftUI
+
+@main
+struct FlohApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
